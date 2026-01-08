7d7b1b80f017d7202deb07b0dc44d28bff2555e3
pdefusco <pauldefusco@gmail.com>
2026-01-07 16:59:37 -0800

    updated field names for app id and launch time to reflect spark task metrics table schema

--- continuous_monitoring_agent/continuousMonitoringAgent.py
+++ continuous_monitoring_agent/continuousMonitoringAgent.py
@@ -367,10 +367,10 @@ def agent_loop():
         if last_launch_time:
             where_clause = f"""
             WHERE (
-                job_launch_time > TIMESTAMP('{last_launch_time}')
+                launchTime > TIMESTAMP('{last_launch_time}')
                 OR (
-                    job_launch_time = TIMESTAMP('{last_launch_time}')
-                    AND spark_application_id > '{last_app_id}'
+                    launchTime = TIMESTAMP('{last_launch_time}')
+                    AND appId > '{last_app_id}'
                 )
             )
             """
